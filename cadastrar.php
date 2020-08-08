233bb940a9390616f9a85bad05d4b5bd36ca3bee
Rene Michel <renemichel856@gmail.com>
2020-08-08 15:09:04 -0300

    atualizando o cadastrar e adicionando login

--- cadastrar.php
+++ cadastrar.php
@@ -9,63 +9,68 @@
 	foreach ($_POST as $post)
 		$json = $post;
 
-	$json = json_decode($json, true);
-
-	$nome = $json['nome'];
-	$matricula = $json['matricula'];
-	$email = $json['email'];
-	$curso = $json['curso'];
-	$ano = $json['ano'];
-	$senha = $json['senha'];
-	$token = uniqid();
-
-	if($nome && $matricula && $email && $curso && $ano && $senha)
+	if($json!=null)
 	{
-		$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB']);
+		$json = json_decode($json, true);
 
-		//checa se o email já existe
-		$query = new MongoDB\Driver\Query(['email'=>$email]);
-		//checa se a matricula já existe
-		$query2 = new MongoDB\Driver\Query(['matricula'=>$matricula]);
+		$nome = $json['nome'];
+		$matricula = $json['matricula'];
+		$email = $json['email'];
+		$curso = $json['curso'];
+		$ano = $json['ano'];
+		$senha = $json['senha'];
+		$token = uniqid();
 
-		$rows = $manager->executeQuery("pds.usuario",$query);
-		$rows2 = $manager->executeQuery("pds.usuario",$query2);
-		$count = 0;
-		foreach ($rows as $row)
-			$count++;
-		foreach ($rows2 as $row)
-			$count++;
-
-		if($count==0)
+		if($nome && $matricula && $email && $curso && $ano && $senha)
 		{
-			//$input = 'python Send.py "'.$email.'" "'.$nome.'" "'.$token.'"';
-			$output = "email sent";
-			//exec($input,$output);
-			//$output = implode($output);
-			if ($output=="email sent")
-			{
-				$bulk = new MongoDB\Driver\BulkWrite;
-				$account = array(
-					'_id'=>uniqid(),
-					'nome'=>$nome,
-					'cargo'=>"aluno",
-					'matricula'=>$matricula,
-					'email'=>$email,
-					'curso'=>$curso,
-					'ano'=>$ano,
-					'senha'=>$senha,
-					'horas'=>0,
-					'certificados'=>[],
-					'validate'=>$token);
-				$bulk->insert($account);
+			$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB']);
 
-				$res = $manager->executeBulkWrite("pds.usuario",$bulk);
-				echo json_encode(array("status"=>"success"));
+			//checa se o email já existe
+			$query = new MongoDB\Driver\Query(['email'=>$email]);
+			//checa se a matricula já existe
+			$query2 = new MongoDB\Driver\Query(['matricula'=>$matricula]);
+
+			$rows = $manager->executeQuery("pds.usuario",$query);
+			$rows2 = $manager->executeQuery("pds.usuario",$query2);
+			$count = 0;
+			foreach ($rows as $row)
+				$count++;
+			foreach ($rows2 as $row)
+				$count++;
+
+			if($count==0)
+			{
+				//$input = 'python Send.py "'.$email.'" "'.$nome.'" "'.$token.'"';
+				$output = "email sent";
+				//exec($input,$output);
+				//$output = implode($output);
+				if ($output=="email sent")
+				{
+					$bulk = new MongoDB\Driver\BulkWrite;
+					$account = array(
+						'_id'=>uniqid(),
+						'nome'=>$nome,
+						'cargo'=>"aluno",
+						'matricula'=>$matricula,
+						'email'=>$email,
+						'curso'=>$curso,
+						'ano'=>$ano,
+						'senha'=>$senha,
+						'horas'=>0,
+						'certificados'=>[],
+						'validate'=>$token);
+					$bulk->insert($account);
+
+					$res = $manager->executeBulkWrite("pds.usuario",$bulk);
+					echo json_encode(array("status"=>"success"));
+				}
 			}
+			else
+				echo json_encode(array("status"=>"duplicate_account"));
 		}
 		else
-			echo json_encode(array("status"=>"duplicate_account"));
+			echo json_encode(array("status"=>"none_data"));
 	}
-	else
-		echo json_encode(array("status"=>"none_data"));	
+	echo json_encode(array("status"=>"null"));
+	
 ?>
---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+	header("Access-Control-Allow-Origin:*");
+	header("Access-Control-Allow-Headers:*");
+	header("Content-type: application/json");
+
+	$json = null;
+	foreach ($_POST as $post)
+		$json = $post;
+
+	if($json!=null)
+	{
+		$json = json_decode($json,true);
+
+		$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB']);
+		$query = new MongoDB\Driver\Query(['_id'=>$json['_id']]);
+
+		$rows = $manager->executeQuery("pds.usuario",$query);
+
+		$acc = null;
+
+		foreach($rows as $row)
+			$acc = $row;
+
+		if($acc==$json['senha'])
+		{
+			$account = array(
+				'_id'=>$acc->_id,
+				'cargo'=>$acc->cargo,
+				'nome'=>$acc->nome,
+				'matricula'=>$acc->matricula,
+				'email'=>$acc->email,
+				'curso'=>$acc->curso,
+				'ano'=>$acc->ano,
+				'senha'=>$acc->senha,
+				'horas'=>$acc->horas,
+				'certificados'=>$acc->certificados,
+				'validate'=>$acc->validate
+			);
+			echo json_encode($account);
+		}
+		else
+			echo json_encode(array("status"=>"incorrect password"));
+	}
+	else
+		echo json_encode(array("status"=>"null"));
+
+?>
