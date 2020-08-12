06808b7291eef5de7ca85671a9d1adf2066c7cda
Rene Michel <renemichel856@gmail.com>
2020-08-11 22:49:19 -0300

    Cadastro de eventos

---
+++ cadEvento.php
@@ -0,0 +1,38 @@
+<?php
+	header("Access-Control-Allow-Origin:*");
+	header("Access-Control-Allow-Headers:*");
+	header("Content-type: application/json");
+
+	$json = null;
+
+	foreach ($_POST as $post)
+		$json = $post;
+
+	if($json!=null)
+	{
+		$json = json_decode($json,true);
+
+		$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB']);
+
+		$evento = array(
+			'_id'=>uniqid(),
+			'tipo'=>$json['tipo'],
+			'cadastradoPor'=>$json['cadastradoPor'],
+			'tema'=>$json['tema'],
+			'descricao'=>$json['descricao'],
+			'cursos'=>$json['cursos'],
+			'horas'=>$json['horas'],
+			'inicio'=>$json['inicio'],
+			'fim'=>$json['fim'],
+			'data'=>$json['data']
+		);
+
+		$bulk = new MongoDB\Driver\BulkWrite;
+		$bulk->inser($evento);
+
+		$res = $bulk->executeBulkWrite("pds.eventos",$bulk);
+		echo json_encode(array("status"=>"success"));
+	}
+	else
+		echo json_encode(array("status"=>"null"));
+?>
