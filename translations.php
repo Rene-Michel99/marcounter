aa912faebbc049e41c77816acf5b4b6b3b7d663e
Renê Michel <55860702+Rene-Michel99@users.noreply.github.com>
2021-02-07 14:09:05 -0300

    Update translations.php

--- translations.php
+++ translations.php
@@ -12,7 +12,7 @@
 		$json = json_decode($json,true);
 
 		$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB2']);
-		$query = new MongoDB\Driver\Query([]);
+		$query = new MongoDB\Driver\Query(['_id'=>'001']);
 		$rows = $manager->executeQuery("bot_translator.status",$query);
 
 		$response = null;
@@ -22,17 +22,7 @@
 		
 		echo $response;
 		
-		$response["status"] = $json["status"];
-		$response["activity_now"] = json["now"];
-		$response["list_success"] = json["list_suc"];
-		$response["fails_list"] = json["list_fails"];
-		$response["fails_translations"] = json["fail_tr"];
-		$response["success_translations"] = json["suc_tr"];
-    
-		$bulk = new MongoDB\Driver\BulkWrite;
-		$bulk->update(['_id'=>$response->_id],$response);
-
-		$res = $manager->executeBulkWrite("bot_translator.status",$bulk);
+		
     		echo "success";
 	}
 	else
